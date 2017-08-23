4d939ffe36fa48662405fb482a8d10ea70812c7a
j3dev <coffeekrakn@gmail.com>
2017-08-23 12:32:24 -0400

    added theme changer and updated some image assets to look a little nicer, although still rough

--- js/ttt.js
+++ js/ttt.js
@@ -1,28 +1,49 @@
 window.addEventListener('load', function() {
+  // player model
   class Player {
     constructor() {
       this.symbol = '../assets/x.png';
       this.theme = ['../assets/x.png', '../assets/o.png'];
+      this.prevTheme = [];
     }
 
     changeTheme() {
+      // change board themes by calling this method
       const themes = [
         ['../assets/x.png', '../assets/o.png'],
         ['../assets/javascript.png', '../assets/python.png'],
         ['../assets/rainbowdash.png', '../assets/twilight.png'],
         ['../assets/rdcutiemark.png', '../assets/tcutiemark.png']
       ];
-      let currentThemeIndex = themes.indexOf(this.theme);
 
+      let currentThemeIndex;
+      let currentSymbolIndex = this.theme.indexOf(this.symbol);
+
+      // can't compare arrays directly so will use forEach()
+      // should consider if using an object is more appropriate
+      // for themes than an array
+      themes.forEach((theme, ti) => {
+        if (theme[0] === this.theme[0]) {
+          currentThemeIndex = ti;
+        }
+      });
+
+      // set prev theme to current theme
+      this.prevTheme = this.theme;
+
+      // now set current theme to next theme in themes array
       if(currentThemeIndex === themes.length-1) {
         this.theme = themes[0];
       } else {
         this.theme = themes[currentThemeIndex+1];
       }
-      this.symbol = this.theme[0];
+
+      // set current symbol to new path
+      this.symbol = this.theme[currentSymbolIndex];
     }
 
     changeSymbol() {
+      // switch symbols on each turn by calling this method
       let currentSymbolIndex = this.theme.indexOf(this.symbol);
       let newSymbolIndex = (currentSymbolIndex === 0) ? 1 : 0;
       this.symbol = this.theme[newSymbolIndex];
@@ -30,10 +51,22 @@ window.addEventListener('load', function() {
   }
 
 
-  // player is like a global var for the game
+  // these are like global vars for the game
   var player;
+  var newBoard;
+  const htmlBoard = document.getElementById('board');
+  const clearBtn = document.createElement('div');
+  const themeBtn = document.createElement('div');
+
+  clearBtn.id = 'clear_btn'
+  clearBtn.className = 'game_btn'
+  clearBtn.innerText = 'Clear';
+  themeBtn.id = 'theme_btn'
+  themeBtn.className = 'game_btn'
+  themeBtn.innerText = 'Theme';
 
 
+  // board model
   class Board {
     constructor(size) {
       this.size = size;
@@ -45,6 +78,7 @@ window.addEventListener('load', function() {
       const board = document.getElementById('board');
       const rows = this.assignStyleClasses();
 
+      // for each row append it to the board container
       rows.forEach((row, si) => {
         return board.appendChild(row);
       });
@@ -53,6 +87,8 @@ window.addEventListener('load', function() {
     assignStyleClasses() {
       const spaces = [];
 
+      // here we check space position and add appropriate class names
+      // which is for border styling
       for(let ri = 0; ri < this.size; ri++) {
         const rowCont = document.createElement('div');
         rowCont.className = 'board_row';
@@ -72,9 +108,13 @@ window.addEventListener('load', function() {
       			htmlClass.push('last_col');
       		}
 
+          // create the space object
           const space = new Space();
+          // push it to the space array so we can easily update
+          // extant space objects later
           this.spacesArray.push(space);
 
+          // create and append space elements to row container
           rowCont.appendChild(space.createSpace(htmlClass.join(' ')));
         };
 
@@ -82,16 +122,21 @@ window.addEventListener('load', function() {
         spaces.push(rowCont);
       };
 
+      // return an array of row elements
       return spaces;
     }
 
     checkForEmptySpaces() {
+      // filter for empty spaces
       const emptySpaces = this.spacesArray.filter((space) => {
         if (!space.occupied) {
           return true;
         }
       });
 
+      // if there are empty spaces then simply return true
+      // else update UI with game over, update board object
+      // with finished = true and return false
       if (emptySpaces.length > 0) {
         return true;
       } else {
@@ -110,9 +155,12 @@ window.addEventListener('load', function() {
   }
 
 
+  // space model
   class Space {
     constructor() {
       this.occupied = false;
+      this.symbol = '';
+      this.element = '';
     }
 
     createSpace(htmlClass) {
@@ -126,30 +174,26 @@ window.addEventListener('load', function() {
         if(!self.occupied) {
           // update the space, style, and player turn
           self.occupied = true;
-          e.target.style.background = 'no-repeat center/93% url(' + player.symbol + ')';
+          self.symbol = player.symbol;
+          e.target.style.background = 'no-repeat center/80% url(' + player.symbol + ')';
           player.changeSymbol();
         }
       });
 
+      this.element = spaceCont;
       return spaceCont;
+    }
+
+    changeTheme() {
+      this.element.style.background = 'no-repeat center/80% url(' + this.symbol + ')';
     }
   }
 
 
-  const htmlBoard = document.getElementById('board');
-  const clearBtn = document.createElement('div');
-  const themeBtn = document.createElement('div');
-
-  clearBtn.id = 'clear_btn'
-  clearBtn.className = 'game_btn'
-  clearBtn.innerText = 'Clear';
-  themeBtn.id = 'theme_btn'
-  themeBtn.className = 'game_btn'
-  themeBtn.innerText = 'Theme';
-
+  // create or re-create new game
   let createNewGame = () => {
     player = new Player();
-    const newBoard = new Board(3);
+    newBoard = new Board(3);
 
     newBoard.placeSpaces();
     htmlBoard.appendChild(clearBtn);
@@ -162,7 +206,10 @@ window.addEventListener('load', function() {
     });
   };
 
+
+  // call the create new game function (on page load)
   createNewGame();
+
 
   // add clear board listener and button
   clearBtn.addEventListener('click', () => {
@@ -170,13 +217,29 @@ window.addEventListener('load', function() {
     while (htmlBoard.firstChild) {
       htmlBoard.removeChild(htmlBoard.firstChild);
     }
+
     // make new board and create new player object
     createNewGame();
   });
 
+
   // add change theme listener and button
   themeBtn.addEventListener('click', () => {
+    // change the theme at player level
+    player.changeTheme();
 
+    newBoard.spacesArray.forEach((space) => {
+      let prevSymbol = space.symbol;
+      let prevSymbolIndex = player.prevTheme.indexOf(prevSymbol);
+
+      // update space symbol
+      space.symbol = player.theme[prevSymbolIndex];
+
+      // change the space's background only if it's occupied
+      if(space.occupied) {
+        space.changeTheme();
+      }
+    });
   });
 
 }, false);
